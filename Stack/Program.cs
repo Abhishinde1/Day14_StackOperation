42d6738856f6d583f6f9abbee97577cfb6ec9532
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-14 17:10:48 +0530

    Welcome to StackOperation Programs

---
+++ Stack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            UC1 uc1 = new UC1();
+            uc1.push(70);
+            uc1.push(30);
+            uc1.push(56);
+            uc1.Display();
+        }
+    }
+}
